360e8d209c0a7a1e2539b91afd3ff10e10119e9f
Mice <miceoroni@github.com>
2025-11-28 17:27:41 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# i2hdify - a simple ffmpeg script
+
+## How do i run this?
+depending on your platform, enter one of the folders, then run either stream for a udp stream or video for a mp4 file named
+"input" EXACTLY
+
+## What does this simulate?
+It simulates both the nielsen watermarking and hd output cards from the IntelliStar 2 HD by giving the
+saturated look of the thunderstorm card, and the audio from the nielsen card. this was written as a quick
+thing for people with i2's or simulators can use to help give off a realistic early 2010's i2 era look and feel.
---
+++ windows/video.bat
@@ -0,0 +1,15 @@
+ffmpeg -y -i input.mp4 -filter_complex "
+[0:v]eq=brightness=0.10:saturation=1.21[v];
+sine=frequency=1030:duration=0.125[s1];
+sine=frequency=1230:duration=0.125[s2];
+sine=frequency=1410:duration=0.125[s3];
+sine=frequency=1610:duration=0.125[s4];
+sine=frequency=1800:duration=0.125[s5];
+sine=frequency=1980:duration=0.125[s6];
+sine=frequency=2180:duration=0.125[s7];
+sine=frequency=2410:duration=0.125[s8];
+sine=frequency=2620:duration=0.125[s9];
+sine=frequency=2800:duration=0.125[s10];
+[s1][s2][s3][s4][s5][s6][s7][s8][s9][s10]amix=inputs=10[audio_tones];
+[0:a][audio_tones]amix=inputs=2:weights=1 0.02:duration=first[a]
+" -map "[v]" -map "[a]" -c:v mpeg2video -b:v 6M -c:a aac -shortest output.mp4
